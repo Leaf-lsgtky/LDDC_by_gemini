b260ece49dbeef9a3f74d5c5c2c7bdb0f9d6efa9
Leaf-lsgtky <1587968048@qq.com>
2025-11-21 19:56:08 +0800

    debug日志

--- index.tsx
+++ index.tsx
@@ -2,14 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+console.log("index.tsx execution started");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+try {
+  const rootElement = document.getElementById('root');
+  if (!rootElement) {
+    throw new Error("Could not find root element to mount to");
+  }
+  console.log("Root element found, creating root...");
+
+  const root = ReactDOM.createRoot(rootElement);
+  
+  console.log("Rendering React App...");
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  console.log("React App rendered triggered");
+} catch (e) {
+  console.error("React Mount Error: " + e);
+}
